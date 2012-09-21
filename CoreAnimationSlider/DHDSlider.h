6ed0edd389774538c8e90e20c20078fbd99eaf6b
Douglas Heriot <douglas@douglasheriot.com>
2012-09-21 18:17:54 +1000

    Created slider class

---
+++ CoreAnimationSlider/DHDSlider.h
@@ -0,0 +1,13 @@
+//
+//  DHDSlider.h
+//  CoreAnimationSlider
+//
+//  Created by Douglas Heriot on 21/09/12.
+//  Copyright (c) 2012 Douglas Heriot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DHDSlider : NSControl
+
+@end
---
+++ CoreAnimationSlider/DHDSlider.m
@@ -0,0 +1,28 @@
+//
+//  DHDSlider.m
+//  CoreAnimationSlider
+//
+//  Created by Douglas Heriot on 21/09/12.
+//  Copyright (c) 2012 Douglas Heriot. All rights reserved.
+//
+
+#import "DHDSlider.h"
+
+@implementation DHDSlider
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
