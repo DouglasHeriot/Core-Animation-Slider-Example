4ceda9bdaf28213467548ca7e8e621f00cbdcd8a
Douglas Heriot <douglas@douglasheriot.com>
2012-09-22 23:12:43 +1000

    Simple first steps

--- CoreAnimationSlider/DHDSlider.m
+++ CoreAnimationSlider/DHDSlider.m
@@ -7,6 +7,11 @@
 //
 
 #import "DHDSlider.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface DHDSlider()
+@property (strong) CALayer *thumb;
+@end
 
 @implementation DHDSlider
 
@@ -15,14 +20,50 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code here.
+		
+		self.wantsLayer = YES;
+		
+		self.layer.contents = [NSImage imageWithSize:self.bounds.size flipped:NO drawingHandler:^BOOL(NSRect dstRect) {
+			[[NSColor greenColor] set];
+			NSRectFill(dstRect);
+			return YES;
+		}];
+		
+		NSSize thumbSize = NSMakeSize(self.bounds.size.width, self.bounds.size.width);
+		
+		self.thumb = [CALayer layer];
+		
+		self.thumb.contents = [NSImage imageWithSize:thumbSize flipped:NO drawingHandler:^BOOL(NSRect dstRect) {
+				[[NSColor redColor] set];
+			NSRectFill(dstRect);
+			return YES;
+		}];
+		
+		[self.layer addSublayer:self.thumb];
     }
     
     return self;
 }
 
-- (void)drawRect:(NSRect)dirtyRect
+- (BOOL)wantsUpdateLayer
 {
-    // Drawing code here.
+	return YES;
+}
+
+- (void)updateLayer
+{
+	[CATransaction begin];
+	[CATransaction setDisableActions:YES];
+	
+	NSSize thumbSize = NSMakeSize(self.bounds.size.width, self.bounds.size.width);
+	NSRect thumbFrame;
+	thumbFrame.size = thumbSize;
+	thumbFrame.origin.x = 0.0;
+	thumbFrame.origin.y = 0.0;
+	self.thumb.frame = thumbFrame;
+	
+	[CATransaction commit];
+	
 }
 
 @end
